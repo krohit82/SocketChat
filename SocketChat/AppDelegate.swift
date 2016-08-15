de065039b0df5d8d66bf7eb4f457fbc30cd4299d
Rohit Kabra <krohit82@gmail.com>
2016-08-15 11:54:48 +0530

    Initial commit

--- SocketChat/AppDelegate.swift
+++ SocketChat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -25,6 +26,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
+        
+        
+       SocketIOManager.sharedInstance.closeConnection()
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -34,6 +38,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
+        
+    
+       SocketIOManager.sharedInstance.establishConnection()
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
